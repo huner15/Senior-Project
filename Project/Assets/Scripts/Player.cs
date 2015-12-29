09793e62771d402962f25e658c4482589523cbfa
Alanna Buss <alannabuss@Alannas-MacBook-Air.local>
2015-12-29 10:00:01 -0800

    Added basic are we interacting or not to the z key

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -16,25 +16,63 @@ public class Player : MovingObject {
 	// Update is called once per frame
     void Update () {
         // Move left
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            touchingBuilding = touchingObject = touchingNPC = false;
-            AttemptMove<Player>(-1, 0);
-        }
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			touchingBuilding = touchingObject = touchingNPC = false;
+			AttemptMove<Player> (-1, 0);
+		}
         // Move right
-        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            touchingBuilding = touchingObject = touchingNPC = false;
-            AttemptMove<Player>(1, 0);
-        }
+        else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			touchingBuilding = touchingObject = touchingNPC = false;
+			AttemptMove<Player> (1, 0);
+		}
         // Move up
-        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            touchingBuilding = touchingObject = touchingNPC = false;
-            AttemptMove<Player>(0, 1);
-        }
+        else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			touchingBuilding = touchingObject = touchingNPC = false;
+			AttemptMove<Player> (0, 1);
+		}
         // Move down
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            touchingBuilding = touchingObject = touchingNPC = false;
-            AttemptMove<Player>(0, -1);
-        }
+        else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			touchingBuilding = touchingObject = touchingNPC = false;
+			AttemptMove<Player> (0, -1);
+		}
+		//Interaction
+		else if (Input.GetKeyDown (KeyCode.Z)) {
+			if (tryToInteract()){
+				print("Successful Interaction");
+			}else{
+				print("Unsuccessful Interaction");
+			}
+		}
+	}
+
+	//Sees if there is building or NPC in the surrounding tiles 
+	protected virtual bool tryToInteract (){
+		//Start is where the player is
+		Vector2 start = transform.position; 
+		//Make vectors for the surrounding area.
+		Vector2 left = start + new Vector2 (-1, 0);
+		Vector2 right = start + new Vector2 (1, 0);
+		Vector2 down = start + new Vector2 (0, -1);
+		Vector2 up = start + new Vector2 (0, 1);
+
+		//We don't want to have the player be considered something to interact with so
+		//we turn this off temporarily
+		boxCollider.enabled = false;
+
+		RaycastHit2D leftHit = Physics2D.Linecast (start, left, blockingLayer);
+		RaycastHit2D rightHit = Physics2D.Linecast (start, right, blockingLayer);
+		RaycastHit2D downHit = Physics2D.Linecast (start, down, blockingLayer);
+		RaycastHit2D upHit = Physics2D.Linecast (start, up, blockingLayer); 
+
+		//turn this back on since we have done all of our hit creation.
+		boxCollider.enabled = true;
+
+		//Return true if any of the surrounding area has an object you interact with it
+		if (leftHit != null || rightHit != null || downHit != null || upHit != null) {
+			return true;
+		}
+
+		return false;
 	}
 
     protected override void AttemptMove<T>(int xDir, int yDir) {
@@ -44,6 +82,7 @@ public class Player : MovingObject {
 
         // Try to move in the direction of the input
         base.AttemptMove<T>(xDir, yDir);
+
 
         // Draw the new tile we're on
         map.Undraw(oldX, oldY);
